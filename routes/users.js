2f034068fa2748d29a62a9ffb5d81add8614dd51
niamh <niamh321@me.com>
2024-07-18 12:22:19 +0100

    clean up/refactor

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,5 @@
 const express = require("express");
 
-const multer = require("multer");
-const { s3Uploadv2 } = require("./s3Service");
-
 const UsersController = require("../controllers/users");
 const tokenChecker = require("../middleware/tokenChecker");
 const upload = require("../middleware/multer");
@@ -14,33 +11,11 @@ router.post("/", UsersController.create);
 router.get("/getOneUser", tokenChecker, UsersController.getOneUser);
 router.put("/profile", tokenChecker, UsersController.updateUser);
 
-// router.get("/profile", UsersController.getOneUser)
-// router.get("/profile:{userid}", UsersController.getOneUser)
-
 router.post("/friends", tokenChecker, UsersController.addFriend);
 router.delete("/friends", tokenChecker, UsersController.removeFriend);
 
-//comenting out for now to try new multer way with AWS
-// router.post(
-//   "/profilePicture",
-//   tokenChecker,
-//   upload.single("profilePicture"),
-//   UsersController.addProfilePicture
-// );
-
-//from tutorial, will need to move function into controllers
-router.post("/upload", upload.array("file"), async (req, res) => {
-  try {
-    const results = await s3Uploadv2(req.files);
-    console.log(results);
-    return res.json({ status: "success" });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-
-
+//allows upload of multiple files to /upload folder in AWS bucket 
+router.post("/upload", upload.array("file"), UsersController.addProfilePicture);
 // router.delete("/profilePicture", tokenChecker, UsersController.removeProfilePicture);
 
 router.delete("/friends/deny", tokenChecker, UsersController.denyFriend);
